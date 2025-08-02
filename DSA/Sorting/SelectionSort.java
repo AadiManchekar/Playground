fc3759576a66cb0706f36e4c18348fb34bcf61c6
Aadi Manchekar <aadimanchekar2002@gmail.com>
2025-08-02 21:58:18 +0530

    Add SelectionSort

---
+++ DSA/Sorting/SelectionSort.java
@@ -0,0 +1,24 @@
+class SelectionSort {
+    public int[] sortArray(int[] nums) {
+        selectionSort(nums);
+        return nums;
+    }
+
+    private void selectionSort(int[] nums) {
+        // assume first element is small, find smallest element and swap with first element
+        // increment first;
+        int n = nums.length - 1;
+        for(int i=0; i <= n-1; i++) {
+            int smallestElement = nums[i];
+            int smallestElementIndex = i;
+            for(int j = i; j <= n; j++) {
+                if(nums[j] < smallestElement) {
+                    smallestElement = nums[j];
+                    smallestElementIndex = j;
+                }
+            }
+            nums[smallestElementIndex] = nums[i];
+            nums[i] = smallestElement;
+        }
+    }
+}
